9813a5d697268037ae8e6cd538c03f6b02cd5ecf
sriharshabsprasad <143907392+sriharshabsprasad@users.noreply.github.com>
2025-09-12 23:04:35 -0500

    fixing git bugs

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  darkMode: 'class', // enables manual dark mode toggle
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
